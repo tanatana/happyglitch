7700f97b69b152f9536c0bf1a9836dd4dc686337
tanatana <tugusuke@gmail.com>
2013-06-02 20:00:34 +0900

    ver 0.1.0

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,14 +10,51 @@
         // ロード時の描画処理
         // 画像のサイズ調整・位置調整はここでする
         $(tmpImg).bind('load', function(){
-            var ctx  = $('.front').find('[data-canvas-id=' + currCanvasId +']')[0].getContext('2d');
+            var natWidth  = tmpImg.width;
+            var natHeight = tmpImg.height;
+            var fCanvW = $('.front').find('canvas').attr('width');
+            var fCanvH = $('.front').find('canvas').attr('height');
+            var bCanvW = $('#background').find('canvas').attr('width');
+            var bCanvH = $('#background').find('canvas').attr('height');
+            var fOpts  = {'dx': 0, 'dy': 0, 'dw': 0, 'dh':0};
+            var bOpts  = {'dx': 0, 'dy': 0, 'dw': 0, 'dh':0};
+            
+            // 補正情報の計算
+            // キャンバスアスペクト比による補正
+            if ((bCanvH / bCanvW) <= 1.0){
+                console.log('width priority');
+                // virtural Background Height
+                var vbHeight = natHeight * (bCanvW/natWidth);
+                var vfHeight = natHeight * (fCanvW/natWidth);                
+                bOpts.dy = ((vbHeight - bCanvH) / 2.0) * -1.0;
+                bOpts.dw = bCanvW;
+                bOpts.dh = vbHeight;
+                fOpts.dy = ((vbHeight - fCanvH) / 2.0) * -1.0;
+                fOpts.dw = fCanvW;
+                fOpts.dh = vfHeight;
+            }else{
+                console.log('height priority');
+                var vbWidth = natWidth * (bCanvW/natWidth);
+                var vfWidth = natHeight * (fCanvW/natWidth);
+                bOpts.dx = ((vbWidth - bCanvW) / 2.0) * -1.0;
+                bOpts.dw = vbWidth;
+                bOpts.dh = bCanvW;
+                fOpts.dx = ((vfWidth - fCanvW) / 2.0) * -1.0;
+                fOpts.dw = vfWidth;
+                fOpts.dh = fCanvW;
+            };
+
+            console.log(fOpts.dx+', '+fOpts.dy+', '+fOpts.dw+', '+fOpts.dh);
+            console.log(bOpts.dx+', '+bOpts.dy+', '+bOpts.dw+', '+bOpts.dh);
+            
+            var ctx  = $('.front').find('canvas')[0].getContext('2d');
             var ctxb = $('#background').find('[data-canvas-id=' + currCanvasId +']')[0].getContext('2d');
 
             // FIXME: 最適なサイズに
-            ctx.clearRect(0, 0, 1000, 1000);
-            ctxb.clearRect(0, 0, 1000, 1000);
-            ctx.drawImage(tmpImg, 0, 0);
-            ctxb.drawImage(tmpImg, 0, 0);
+            ctx.clearRect( 0, 0, fCanvW, fCanvH);
+            ctxb.clearRect(0, 0, bCanvW, fCanvH);
+            ctx.drawImage( tmpImg, fOpts.dx, fOpts.dy, fOpts.dw, fOpts.dh);
+            ctxb.drawImage(tmpImg, bOpts.dx, bOpts.dy, bOpts.dw, bOpts.dh);
             currCanvasId = (currCanvasId + 1) % 7;
         });
 
@@ -33,8 +70,10 @@
                 fileReader.readAsDataURL(sendFile);
                 fileReader.onload = (function(e){
                     console.log('fr.onload :' + (new Date()).getTime());
+
+                    // ImageResize
                     $.canvasResize(sendFile, {
-                        width: 1600,
+                        width: 1200,
                         height: 0,
                         crop: false,
                         quality: 67,
@@ -100,6 +139,23 @@
     };
     
 
+    var bgCtrl = (function(){
+        var cid = 0;
+        var global = {
+            next: (function(){
+                cid = cid + 1;
+                if(cid >= 7){ cid = 0; }
+                $('#background').find('canvas').css({'left' : $(window).width()});
+                $('#background').find('[data-canvas-id="' + cid +'"]').css({'left' : 0});
+                
+            }),
+            getCid: (function(){
+                return cid;
+            })
+        };
+        return global;
+    })();
+    
     $(document).ready(function(){
         var logosrc = '/images/logo'+ Math.floor( Math.random() * 6 ) + '.jpg';
         $('h1').find('#logo').attr({'src': logosrc});
@@ -112,5 +168,9 @@
             console.log('resize');
             windowResize(e);
         });
+
+        setInterval(function(){
+            bgCtrl.next();
+        }, 100);
     });
 })();
